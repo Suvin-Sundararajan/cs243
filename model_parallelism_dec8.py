a0bc36e97358b113f8e69ca54cf4bede87a86fbc
Suvin-Sundararajan <111387820+Suvin-Sundararajan@users.noreply.github.com>
2023-12-08 08:53:28 -0500

    Update model_parallelism_dec8.py

--- model_parallelism_dec8.py
+++ model_parallelism_dec8.py
@@ -12,74 +12,164 @@ import torch.nn.functional as F
 import sys
 import argparse
 
+
+
 def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12345'
+    os.environ['MASTER_ADDR'] = '172.31.10.39'  # Change with ip address
+    os.environ['MASTER_PORT'] = '29500'
+    print("starting")
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    print("Rank successfully connected to the master node at " + os.environ['MASTER_ADDR'] + ":" + os.environ['MASTER_PORT'])
+
+class SplitResNet(nn.Module):
+    def __init__(self, original_model, split_layer):
+        super(SplitResNet, self).__init__()
+        self.part1 = nn.Sequential(*list(original_model.children())[:split_layer])
+        self.part2 = nn.Sequential(*list(original_model.children())[split_layer:])
+
+    def forward_part1(self, x):
+        return self.part1(x)
+
+    def forward_part2(self, x):
+        return self.part2(x)
+
 
 def cleanup():
     dist.barrier() 
     dist.destroy_process_group()
 
-class ModelParallelResNet50(nn.Module):
-    def __init__(self, split_size, *args, **kwargs):
-        super(ModelParallelResNet50, self).__init__()
-        self.split_size = split_size
-
-        # First part of ResNet50
-        self.model_part1 = nn.Sequential(*list(models.resnet50(pretrained=True).children())[:self.split_size]).cuda(0)
-        
-        # Second part of ResNet50
-        self.model_part2 = nn.Sequential(*list(models.resnet50(pretrained=True).children())[self.split_size:]).cuda(1)
-
-    def forward(self, x):
-        inputs1 = x.to('cuda:0')
-        inputs2 = self.model_part1(inputs1).to('cuda:1')
-        outputs = self.model_part2(inputs2)
-        return outputs
-
-def train(rank, world_size, epochs):
-    print(f"Running on rank {rank}.")
+def main(rank, world_size, use_big_resnet, num_epochs, checkpoint_frequency, save_checkpoint_to_cpu, checkpoint_groups):
+    print(f"Running the basic Model Parallelism example on rank {rank} out of {world_size} processes")
     setup(rank, world_size)
+    device = torch.device(f"cuda:{0}" if torch.cuda.is_available() else "cpu")
+    original_model = models.resnet50(pretrained=True)
+    split_layer = 5  # Choose a layer to split the model
+    split_model = SplitResNet(original_model, split_layer)
+    if rank == 0:
+        model_part = split_model.part1
+    else:
+        model_part = split_model.part2
+    # Define a transform to normalize the data
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    dataset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
-    train_sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank)
-    train_loader = DataLoader(dataset, batch_size=64, shuffle=False, sampler=train_sampler)
+    # Download and load the CIFAR-100 training data
+    trainset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+    train_sampler = DistributedSampler(trainset, num_replicas=world_size, rank=rank)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=False, sampler=train_sampler, pin_memory=False, num_workers=0)
+    find_unused_parameters=True
+    # Initialize ResNet
+    resnet = models.resnet152(pretrained=True) if use_big_resnet else models.resnet50(pretrained=True)
+    
+    num_classes = 100  # CIFAR-100 has 100 classes
+    resnet.fc = torch.nn.Linear(resnet.fc.in_features, num_classes)
+    resnet.to(device)
+    print("ResNet initialized")
+    resnet = DDP(resnet, device_ids=[0], output_device=0, find_unused_parameters=True)
+    print("DDP initialized")
 
-    split_size = 5
-    model = ModelParallelResNet50(split_size=split_size).to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.01, momentum=0.9)
 
-    for epoch in range(epochs):
-        ddp_model.train()
-        for i, (inputs, labels) in enumerate(train_loader):
-            inputs, labels = inputs.to(rank), labels.to(rank)
+    # Optimizer setup
+    optimizer = optim.SGD(resnet.parameters(), lr=0.01, momentum=0.9)
+
+    # Training loop
+    for epoch in range(num_epochs):
+        resnet.train()
+        train_sampler.set_epoch(epoch)
+        for i, (inputs, labels) in enumerate(trainloader):
+            inputs, labels = inputs.to(device), labels.to(device)
+
             optimizer.zero_grad()
-            outputs = ddp_model(inputs)
-            loss = criterion(outputs, labels)
+            outputs = resnet(inputs)
+            loss = F.cross_entropy(outputs, labels)
             loss.backward()
             optimizer.step()
 
             if rank == 0 and i % 10 == 0:
-                print(f"Epoch {epoch}, Step {i}, Loss: {loss.item()}")
+                print(f"Epoch {epoch + 1}, Batch {i}, Loss: {loss.item()}")
+
+        # Checkpointing logic (only by rank 0)
+        if rank == 0 and (epoch + 1) % checkpoint_frequency == 0:
+            continue
+
+    if rank == 0:
+        print("Training Complete")
 
     cleanup()
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--rank', type=int, help='Rank of the process')
-    args = parser.parse_args()
 
-    world_size = 2
-    epochs = 5
+params_per_chunk = 2561000
+def shard_model(model):
+    chunks = []
+    current_chunk_params = {}
+    items = model.state_dict().items()
+    for i in range(len(items)):
+        key, value = list(items)[i]
+        current_chunk_size = sum(p.numel() for p in current_chunk_params.values())
+        if current_chunk_size + value.numel() < params_per_chunk:
+            current_chunk_params[key] = value
+        else:
+            chunks.append(current_chunk_params)
+            # Move to the next chunk
+            current_chunk_params = {}
+            current_chunk_params[key] = value
+    return chunks
 
-    os.environ['NCCL_DEBUG'] = 'INFO'
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+# Load the pretrained ResNet50 model
+model = models.resnet50(pretrained=True)
+model = torch.nn.DataParallel(model)
 
-    train(args.rank, world_size, epochs)
+
+# Call the shard() function
+print(len(shard_model(model)))
 
 if __name__ == "__main__":
-    main()
+    world_size = 2
+    # world_size = torch.cuda.device_count()
+
+    # Check if the expected number of GPUs (4) is available
+    # expected_gpu_count = 1
+    # assert world_size == expected_gpu_count, f"Expected {expected_gpu_count} GPUs, but found {world_size}"
+
+    use_big_resnet = False
+    num_epochs = 5
+    checkpoint_frequency = 2
+    save_checkpoint_to_cpu = True
+    rank = 0
+
+    #TODO: change to call placement_strategy
+    checkpoint_groups = [[0, 1], [2, 3]]  
+
+    if len(sys.argv) > 1:
+        try:
+            num_epochs = int(sys.argv[1])
+        except ValueError:
+            print("Usage: python script.py <number_of_epochs>")
+            sys.exit(1)
+
+    if len(sys.argv) > 2:
+        try:
+            rank = int(sys.argv[2])
+        except ValueError:
+            print("Usage: python script.py <number_of_epochs> <rank>")
+            sys.exit(1)
+    else:
+        print("Usage: python script.py <number_of_epochs> <rank>")
+        sys.exit(1)
+
+    os.environ['NCCL_DEBUG'] = 'INFO'
+    main(rank, world_size, use_big_resnet, num_epochs, checkpoint_frequency, save_checkpoint_to_cpu, checkpoint_groups)    
+
+
+
+
+# Load the pretrained ResNet50 model
+model = models.resnet50(pretrained=True)
+model = torch.nn.DataParallel(model)
+
+
+
+def testing_parameters():
+    return 0
